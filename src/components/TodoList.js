dd062036eec78758997ef7dfec5b50cb15e61cf7
nico <n>
2020-01-30 15:53:23 -0500

    We enable to read data from the store by calling connect with the mapStateToProps parameter

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Todo from "./Todo";
 
+import { connect } from "react-redux";
+
 const TodoList = ({ todos }) => (
   <ul className="todo-list">
     {todos && todos.length
@@ -11,4 +13,13 @@ const TodoList = ({ todos }) => (
   </ul>
 );
 
-export default TodoList;
+const mapStateToProps = state => {
+  const { byIds, allIds } = state.todos || {};
+  const todos =
+    allIds && allIds.length
+      ? allIds.map(id => (byIds ? { ...byIds[id], id } : null))
+      : null;
+  return { todos };
+};
+
+export default connect(mapStateToProps)(TodoList);
