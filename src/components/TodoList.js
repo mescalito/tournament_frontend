121aaca1e0ebb8a10c24228b6200109ac7f6846f
nico <n>
2020-01-30 23:25:14 -0500

    add filters for: all, complete and incomplete

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,7 +2,7 @@ import React from "react";
 import Todo from "./Todo";
 
 import { connect } from "react-redux";
-import { getTodos } from "../redux/selectors";
+import { getTodosByVisibilityFilter } from "../redux/selectors";
 
 const TodoList = ({ todos }) => (
   <ul className="todo-list">
@@ -14,4 +14,10 @@ const TodoList = ({ todos }) => (
   </ul>
 );
 
-export default connect(state => ({ todos: getTodos(state) }))(TodoList);
+const mapStateToProps = state => {
+  const { visibilityFilter } = state
+  const todos = getTodosByVisibilityFilter(state, visibilityFilter)
+  return { todos }
+}
+
+export default connect(mapStateToProps)(TodoList)
